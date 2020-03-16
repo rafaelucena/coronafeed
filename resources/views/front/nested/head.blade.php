026d6fa8da2a6d155ad6c7a567eb282d49eaf785
rboszko <rafa@hotmail.sk>
2020-03-16 22:52:46 +0100

    "#00005 - break layout into small files (More files nested)"

---
+++ resources/views/front/nested/head.blade.php
@@ -0,0 +1,19 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Bolby - Portfolio/CV/Resume HTML Template</title>
+<meta name="description" content="Bolby - Portfolio/CV/Resume HTML Template">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" type="image/x-icon" href="https://via.placeholder.com/32x32">
+
+<!-- STYLESHEETS -->
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/all.min.css') }}">
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/simple-line-icons.css') }}">
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/slick.css') }}">
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/animate.css') }}">
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/magnific-popup.css') }}">
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/style.css') }}">
+
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
---
+++ resources/views/front/nested/scripts.blade.php
@@ -0,0 +1,16 @@
+<script type="text/javascript" src="{{ asset('front/js/jquery-1.12.3.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/jquery.easing.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/jquery.waypoints.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/jquery.counterup.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/isotope.pkgd.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/infinite-scroll.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/slick.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/contact.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/validator.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/wow.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/morphext.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/parallax.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/jquery.magnific-popup.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('front/js/custom.js') }}"></script>
