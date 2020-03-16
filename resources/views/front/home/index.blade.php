9b4b70ff47041ad997c1673c33372af89990bdb9
rboszko <rafa@hotmail.sk>
2020-03-16 22:11:09 +0100

    "#00005 - break layout into small files (renaming folder)"

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -3,47 +3,47 @@
 @section('content')
     <!-- section home -->
 	<section id="home" class="home d-flex align-items-center">
-        @include('front.home.includes.home')
+        @include('front.home.sections.home')
 	</section>
 
 	<!-- section about -->
 	<section id="about">
-		@include('front.home.includes.about')
+		@include('front.home.sections.about')
 	</section>
 
 	<!-- section services -->
 	<section id="services">
-        @include('front.home.includes.services')
+        @include('front.home.sections.services')
 	</section>
 
 	<!-- section experience -->
 	<section id="experience">
-        @include('front.home.includes.experience')
+        @include('front.home.sections.experience')
 	</section>
 
 	<!-- section works -->
 	<section id="works">
-        @include('front.home.includes.works')
+        @include('front.home.sections.works')
 	</section>
 
 	<!-- section prices -->
 	<section id="prices">
-        @include('front.home.includes.prices')
+        @include('front.home.sections.prices')
 	</section>
 
 	<!-- section testimonials -->
 	<section id="testimonials">
-        @include('front.home.includes.testimonials')
+        @include('front.home.sections.testimonials')
 	</section>
 
 	<!-- section blog -->
 	<section id="blog">
-        @include('front.home.includes.blog')
+        @include('front.home.sections.blog')
 	</section>
 
 	<!-- section contact -->
 	<section id="contact">
-        @include('front.home.includes.contact')
+        @include('front.home.sections.contact')
 	</section>
 
 	<div class="spacer" data-height="96"></div>
