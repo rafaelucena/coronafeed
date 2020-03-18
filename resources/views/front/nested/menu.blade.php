0d05eb5ccb610d979a228b8a19d70e71f5ae9c2f
wboszko <vieraboschkova@gmail.com>
2020-03-18 22:15:02 +0100

    #00003 - exploring new template (blade changes)

--- resources/views/front/nested/menu.blade.php
+++ resources/views/front/nested/menu.blade.php
@@ -1,9 +1,9 @@
 <ul class="vertical-menu scrollspy">
-    <li class="active"><a href="#home"><i class="icon-home"></i>Home</a></li>
-    <li><a href="#about"><i class="icon-user-following"></i>About</a></li>
-    <li><a href="#services"><i class="icon-briefcase"></i>Services</a></li>
-    <li><a href="#experience"><i class="icon-graduation"></i>Experience</a></li>
-    <li><a href="#works"><i class="icon-layers"></i>Works</a></li>
-    <li><a href="#blog"><i class="icon-note"></i>Blog</a></li>
-    <li><a href="#contact"><i class="icon-bubbles"></i>Contact</a></li>
+    <li class="active"><a href="#home"><i class="fas fa-home"></i>Home</a></li>
+    <li><a href="#about"><i class="fas fa-asterisk"></i>About COVID-19</a></li>
+    <li><a href="#services"><i class="fas fa-microscope"></i>Facts & Myths</a></li>
+    <li><a href="#experience"><i class="fas fa-file-medical"></i>History</a></li>
+    <li><a href="#works"><i class="fas fa-allergies"></i>Recommendations</a></li>
+    <li><a href="#blog"><i class="fas fa-file-medical-alt"></i>News</a></li>
+    <li><a href="#contact"><i class="fas fa-feather-alt"></i>Contact</a></li>
 </ul>
