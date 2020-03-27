386b12001be4df9c15f333f8329baa92128eb0ac
rboszko <rafa@hotmail.sk>
2020-03-27 23:55:16 +0100

    "#00016 - first graphic trials (cleaning changes)"

--- resources/views/front/nested/scripts.blade.php
+++ resources/views/front/nested/scripts.blade.php
@@ -18,12 +18,5 @@
 <!-- graph-->
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
-
-<!--
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="{{ asset('front/js/jquery.flot.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('components/graphs/js/graph.js') }}"></script>
--->
-
 <!-- world map-->
 <script type="text/javascript" src="{{ asset('components/world-map/js/world-map.js') }}"></script>
