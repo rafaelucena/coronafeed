bc2edbeb8eded10ea37ea16540e5538abd825df8
rafaelucena <r.boszko@b4b.net.pl>
2020-04-01 11:33:25 +0200

    #00029 - translating home and menu (fixing spam text and virus layout)

--- resources/views/front/home/sections/home.blade.php
+++ resources/views/front/home/sections/home.blade.php
@@ -6,7 +6,7 @@
 
 				<!-- info -->
 				<h1 class="mb-2 mt-0">CORONAVIRUS 2019</h1>
-				<span>Estou <span class="text-rotating">na Ásia, na América do Sul, na América do Norte, na Europa, na África, na Oceania, EM TODA PARTE</span></span>
+				<span>Estou <span class="text-rotating">no Distrito Federal, no Rio de Janeiro, em São Paulo, em Pernambuco, na Bahia, no Rio Grande do Sul, EM TODA PARTE</span></span>
 
         <!-- social icons -->
         <ul class="social-icons light list-inline mb-0 mt-4">
@@ -17,7 +17,7 @@
  <!--           <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest-p"></i></a></li>-->
         </ul>
     </div>
-    
+
     <!-- scroll down mouse wheel -->
     <div class="scroll-down">
         <a href="#about" class="mouse-wrapper">
@@ -31,19 +31,19 @@
     <!-- parallax layers -->
     <div class="parallax" data-relative-input="true">
         <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.2" class="layer p2">
-        <img src="{{ asset('front/images/cor2.png') }}" widht="35" height="35" data-depth="0.3" class="layer p3">
-        <img src="{{ asset('front/images/cor3.png') }}" widht="40" height="40" data-depth="0.6" class="layer p4">
-        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.2" class="layer p5">
-        <img src="{{ asset('front/images/cor1.png') }}" widht="100" height="100" data-depth="0.5" class="layer p6">
+        <img src="{{ asset('front/images/cor2.png') }}" widht="35" height="35" data-depth="0.4" class="layer p3">
+        <img src="{{ asset('front/images/cor3.png') }}" widht="40" height="40" data-depth="0.7" class="layer p4">
+        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.5" class="layer p5">
+        <img src="{{ asset('front/images/cor1.png') }}" widht="100" height="100" data-depth="0.3" class="layer p6">
         <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.4" class="layer p7">
-        <img src="{{ asset('front/images/cor3.png') }}" widht="26" height="26" data-depth="0.3" class="layer p8">
-        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.3" data-depth-y="-1.30"class="layer p9">
-        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.2" class="layer p10">
-        <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.5" class="layer p11">
-        <img src="{{ asset('front/images/cor3.png') }}" widht="26" height="26" data-depth="0.4" class="layer p12">
-        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.2" class="layer p13">
+        <img src="{{ asset('front/images/cor3.png') }}" widht="26" height="26" data-depth="0.7" class="layer p8">
+        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.6" data-depth-y="-1.30"class="layer p9">
+        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.4" class="layer p10">
+        <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.2" class="layer p11">
+        <img src="{{ asset('front/images/cor3.png') }}" widht="26" height="26" data-depth="0.8" class="layer p12">
+        <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.5" class="layer p13">
         <img src="{{ asset('front/images/cor2.png') }}" widht="26" height="26" data-depth="0.6" class="layer p14">
-        <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.4" class="layer p15">
-        <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.7" class="layer p16">
+        <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.3" class="layer p15">
+        <img src="{{ asset('front/images/cor1.png') }}" widht="26" height="26" data-depth="0.1" class="layer p16">
     </div>
 </div>
