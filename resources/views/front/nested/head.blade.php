51e3aa85fe6bcf3f24a1b35ccd306ac9884ceb26
rafaelucena <r.boszko@b4b.net.pl>
2020-04-05 00:50:00 +0200

    "#00021 - analytic graphs (source files)"

--- resources/views/front/nested/head.blade.php
+++ resources/views/front/nested/head.blade.php
@@ -13,6 +13,8 @@
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/magnific-popup.css') }}">
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset('front/css/style.css') }}">
 
+<!-- //@TODO - to determine if mbd css will be necessary -->
+<!-- <link rel="stylesheet" type="text/css" media="all" href="{{ asset('components/mdb/css/mdb.css') }}"> -->
 <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
