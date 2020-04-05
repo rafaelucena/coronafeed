7fb5b27c7a817a8da5d90c1a6219ecf863d9e8ba
rafaelucena <r.boszko@b4b.net.pl>
2020-04-06 01:17:26 +0200

    "#00041 - add api route to add location numbers (change controller with store function)"

--- app/Http/Controllers/Api/LocationNumbersController.php
+++ app/Http/Controllers/Api/LocationNumbersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Models\Location;
+use App\Http\Models\LocationNumbers;
 use Doctrine\ORM\EntityManager;
 use Illuminate\Http\Request;
 
@@ -42,6 +43,7 @@ class LocationNumbersController extends Controller
      */
     public function update(Request $request, Location $location): array
     {
+        /** @var LocationNumbers $locationNumbers */
         $locationNumbers = $location->getLocationNumbers();
 
         $old = [
@@ -70,6 +72,34 @@ class LocationNumbersController extends Controller
                 'updated' => $locationNumbers->getUpdated()->format('Y-m-d H:i:s'),
             ],
             'old' => $old,
+        ];
+    }
+
+    /**
+     * @param Request $request
+     * @param Location $location
+     * @return array
+     */
+    public function store(Request $request, Location $location): array
+    {
+        $locationNumbers = new LocationNumbers();
+
+        $locationNumbers->setNewCases($request->input('new_cases'));
+        $locationNumbers->setConfirmed($request->input('confirmed'));
+        $locationNumbers->setDeaths($request->input('deaths'));
+        $locationNumbers->setCured($request->input('cured'));
+        $locationNumbers->setLocation($location);
+
+        $this->em->persist($locationNumbers);
+        $this->em->flush();
+
+        return [
+            'name' => $location->getName(),
+            'new_cases' => $locationNumbers->getNewCases(),
+            'confirmed' => $locationNumbers->getConfirmed(),
+            'deaths' => $locationNumbers->getDeaths(),
+            'cured' => $locationNumbers->getCured(),
+            'updated' => $locationNumbers->getUpdated()->format('Y-m-d H:i:s'),
         ];
     }
 }
