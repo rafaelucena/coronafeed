2afda169807bd459a0c91763c28762ab49713447
rafaelucena <r.boszko@b4b.net.pl>
2020-04-06 10:03:05 +0200

    "#00021 - analytic graphs (changing charts service)"

--- app/Http/Services/Location/ChartsService.php
+++ app/Http/Services/Location/ChartsService.php
@@ -4,111 +4,61 @@ namespace App\Http\Services\Location;
 
 use App\Http\Models\Location;
 use App\Http\Models\LocationHistory;
+use App\Http\Models\LocationNumbers;
 use Doctrine\Common\Collections\ArrayCollection;
 
 class ChartsService
 {
     /** @var array **/
-    private $dates;
-
-    /** @var array **/
-    private $confirmed;
-
-    /** @var array **/
-    private $deaths;
-
-    /** @var array **/
-    private $cured;
+    private $lineChart = [
+        'dates' => [
+            'label' => 'Datas',
+            'list' => [],
+        ],
+        'confirmed' => [
+            'label' => 'Confirmados',
+            'list' => [],
+        ],
+        'deaths' => [
+            'label' => 'Mortes',
+            'list' => [],
+        ],
+        'cured' => [
+            'label' => 'Curados',
+            'list' => [],
+        ],
+    ];
 
     /**
      * @param Location $location
      */
     public function __construct(Location $location)
     {
+        $locationNumbers = $location->getLocationNumbers();
         $locationHistoryArray = $location->getLocationHistory();
-        $this->setDates($locationHistoryArray);
-        $this->setConfirmed($locationHistoryArray);
-        $this->setDeaths($locationHistoryArray);
-        $this->setCured($locationHistoryArray);
+
+        $this->setLineChart($locationHistoryArray);
     }
 
     /**
      * @return array
      */
-    public function getDates(): array
+    public function getLineChart(): array
     {
-        return $this->dates;
+        return $this->lineChart;
     }
 
     /**
      * @param ArrayCollection $locationHistoryArray
      * @return void
      */
-    public function setDates(ArrayCollection $locationHistoryArray): void
+    public function setLineChart(ArrayCollection $locationHistoryArray): void
     {
-        $this->dates = [];
         foreach ($locationHistoryArray as $locationHistory) {
-            $this->dates[] = $locationHistory->getDate()->format('Y-m-d');
-        }
-    }
-
-    /**
-     * @return array
-     */
-    public function getConfirmed(): array
-    {
-        return $this->confirmed;
-    }
-
-    /**
-     * @param ArrayCollection $locationHistoryArray
-     * @return void
-     */
-    public function setConfirmed(ArrayCollection $locationHistoryArray): void
-    {
-        $this->confirmed = [];
-        foreach ($locationHistoryArray as $locationHistory) {
-            $this->confirmed[] = $locationHistory->getConfirmed();
-        }
-    }
-
-    /**
-     * @return array
-     */
-    public function getDeaths(): array
-    {
-        return $this->deaths;
-    }
-
-    /**
-     * @param ArrayCollection $locationHistoryArray
-     * @return void
-     */
-    public function setDeaths(ArrayCollection $locationHistoryArray): void
-    {
-        $this->deaths = [];
-        foreach ($locationHistoryArray as $locationHistory) {
-            $this->deaths[] = $locationHistory->getDeaths();
-        }
-    }
-
-    /**
-     * @return array
-     */
-    public function getCured(): array
-    {
-        return $this->cured;
-    }
-
-    /**
-     * @param ArrayCollection $locationHistoryArray
-     * @return void
-     */
-    public function setCured(ArrayCollection $locationHistoryArray): void
-    {
-        $this->cured = [];
-        foreach ($locationHistoryArray as $locationHistory) {
-            $this->cured[] = $locationHistory->getCured();
+            $this->lineChart['dates']['list'][] = $locationHistory->getDate()->format('Y-m-d');
+            $this->lineChart['confirmed']['list'][] = $locationHistory->getConfirmed();
+            $this->lineChart['deaths']['list'][] = $locationHistory->getDeaths();
+            $this->lineChart['cured']['list'][] = $locationHistory->getCured();
         }
     }
 }
