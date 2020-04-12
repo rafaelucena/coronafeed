ba8bf57c94d62253e646ae0da7637b136dfd8d06
rafaelucena <r.boszko@b4b.net.pl>
2020-04-12 22:59:51 +0200

    "#00063 - add helper to get countries constant info (import source and base function)"

--- app/Helpers/Country.php
+++ app/Helpers/Country.php
@@ -6,4 +6,25 @@ class Country
 {
     /** @var array */
     private $countries;
+
+    public function __construct()
+    {
+        $this->countries = json_decode(file_get_contents(base_path('storage/helpers/countries.json')), true);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function getCountryCodeByName(string $name): string
+    {
+        $name = strtolower($name);
+        foreach ($this->countries as $countryInfo) {
+            if ($countryInfo['name'] === $name) {
+                return $countryInfo['iso'];
+            }
+        }
+
+        return '';
+    }
 }
