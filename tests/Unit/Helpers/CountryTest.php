09b8749a02bb0dc4663146ae3438507aa35e9fde
rafaelucena <r.boszko@b4b.net.pl>
2020-04-12 23:27:08 +0200

    "#00063 - add helper to get countries constant info (add new tests)"

--- tests/Unit/Helpers/CountryTest.php
+++ tests/Unit/Helpers/CountryTest.php
@@ -20,4 +20,43 @@ class CountryTest extends TestCase
         $this->assertEquals('br', $countries->getIsoByName('BRAZIL'));
         $this->assertEquals('br', $countries->getIsoByName('BrAzIl'));
     }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function testCountryCodeByNameIsInvalid()
+    {
+        $countries = new Country();
+        $this->assertNotEquals('br', $countries->getIsoByName('bruh'));
+        $this->assertNotEquals('br', $countries->getIsoByName('bearn'));
+        $this->assertNotEquals('br', $countries->getIsoByName('brzila'));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function testCountryCodeByWorldometerIsValid()
+    {
+        $countries = new Country();
+        $this->assertEquals('us', $countries->getIsoByWorldometerName('usa'));
+        $this->assertEquals('us', $countries->getIsoByWorldometerName('USA'));
+        $this->assertEquals('us', $countries->getIsoByWorldometerName('UsA'));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function testCountryCodeByWorldometerIsInvalid()
+    {
+        $countries = new Country();
+        $this->assertNotEquals('us', $countries->getIsoByWorldometerName('usada'));
+        $this->assertNotEquals('us', $countries->getIsoByWorldometerName('ussa'));
+        $this->assertNotEquals('us', $countries->getIsoByWorldometerName('asu'));
+    }
 }
