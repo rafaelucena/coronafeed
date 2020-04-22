c93655afae5430d776b38114384656f822a7ee26
rafaelucena <r.boszko@b4b.net.pl>
2020-04-23 00:27:14 +0200

    "#00076 - add model for languages"

---
+++ app/Http/Models/Language.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Models;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\Mapping AS ORM;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="languages")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Language
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5, nullable=false)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @param string $code
+     * @return void
+     */
+    public function setCode(string $code): void
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $slug
+     * @return void
+     */
+    public function setSlug(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
