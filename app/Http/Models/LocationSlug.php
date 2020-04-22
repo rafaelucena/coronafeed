2ca0878546b0cf442b5297f314a5936105fa885b
rafaelucena <r.boszko@b4b.net.pl>
2020-04-23 00:30:09 +0200

    "#00077 - add model for location slug"

---
+++ app/Http/Models/LocationSlug.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Models;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\Mapping AS ORM;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="location_slug")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class LocationSlug
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @param string $slug
+     * @return void
+     */
+    public function setSlug(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
