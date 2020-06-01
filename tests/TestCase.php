9361d7a52b8f4f3ebb3842c6d9137e07fd15eafd
rboszko <rafa@hotmail.sk>
2020-06-01 22:13:39 +0200

    "#00116 - add changes to main test case to allow reflection (reflection finally)"

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use ReflectionMethod;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $service;
+
+    protected function callPrivate(string $method, $parameters)
+    {
+        $reflectionMethod = new ReflectionMethod(get_class($this->service), $method);
+        $reflectionMethod->setAccessible(true);
+
+        return $reflectionMethod->invoke($this->service, $parameters);
+    }
 }
